f75c09f8697b199739db875b3b9d34c2ac16b035
ninetySixthPixel <ninetysixthpixel@gmail.com>
2020-06-27 14:44:14 +0100

    Initial Commit

---
+++ DiabetesApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiabetesApp
+//
+//  Created by Luke Prescott on 27/06/2020.
+//  Copyright © 2020 ninetySixthPixel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
